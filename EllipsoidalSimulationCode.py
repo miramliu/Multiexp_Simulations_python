e4d0d55e0241708cd9d15e22aa02cc756e38dc8a
Mira Liu <30845365+miramliu@users.noreply.github.com>
2024-01-25 14:23:13 -0500

    ..

--- EllipsoidalSimulationCode.py
+++ EllipsoidalSimulationCode.py
@@ -1,6 +1,27 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as pl
+from scipy.stats import rice
+
+import csv
+
+import scipy.optimize as op
+import scipy.stats
+from scipy.optimize import curve_fit
+import scipy.io
+from scipy.stats import rice
+
+import random
+import pickle
+import seaborn as sns
+
+import numpy as np
+from scipy.stats import ttest_ind, ttest_ind_from_stats, wilcoxon,ttest_rel, pearsonr,shapiro,f_oneway, ranksums
+from scipy.special import stdtr
+import csv
+import pandas as pd
+
+from statistics import mean
 
 # So create ellipsoid, and rotate it to create eigenvalues and eigenvectors (i.e. create anisotropic diffusion ellipsoid)
 # then find Diffusion tensor (knowing eigenvalues and eigenvectors from the ellipsoid)
@@ -166,13 +187,22 @@ def findminidx(x):
     ncol = x.shape[1]
     return k//ncol, k%ncol
 
-
 def Calc_FA(a,b,c):
     MD = (a+b+c)/3
     num = np.sqrt((a-MD)**2 + (b-MD)**2 + (c-MD)**2)
     denom = np.sqrt(a**2 + b**2 + c**2)
     return np.sqrt(3/2)*num/denom
 
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx], idx
+
+def GenerateFA_Ellipsoid(FA_want,FAs,abcs):
+    val,idx = find_nearest(FAs,FA_want)
+    return abcs[idx]#, FA(abcs[idx][0],abcs[idx][1],abcs[idx][2])
+    
 # NEW RICIAN FUNCTION.From old code. double check calculation of SNSR and sigma? 
 #SNR = 1, sigma = 1
 #SNR = 10, sigma = .1
@@ -182,6 +212,7 @@ def Calc_FA(a,b,c):
 #SNR = 100, sigma = .01
 #SNR = 125, sigma = .008
 #SNR = 150, sigma = .0065
+
 def NoiseRice(I,sigma): # noise with rician distribution
     N = [] 
     #v = .79 # calculated from images on 6/11/19
@@ -193,4 +224,114 @@ def NoiseRice(I,sigma): # noise with rician distribution
     for i in range(0,len(I)):
         N.append(I[i] +r[i]) #SNR = 1,10,22,100,150,inf
     return N
+
+def triexp_func(b, frac_fast,frac_med,frac_slow,diff_fast,diff_med,diff_slow):
+    return (1-frac_med-frac_fast)*np.exp(-b*diff_slow)+frac_med*np.exp(-b*diff_med)+frac_fast*np.exp(-b*diff_fast)
+
+def Scale_Ds(D_traces,abc_s,thetas):
+    D_thetas = np.zeros(len(D_traces))
+    for n in range(len(D_traces)): #here n is fast, medium, or slow compartments. i.e. the 3 ellipsoids
+        D_n = 3*D_traces[n]/sum(abc_s[n]) #normalize to ellipsoid (basically normalize to 1)
+        D_thetas[n] = D_n*thetas[n] #multiply by diffusion strength along b-value direction 
+    return D_thetas #return the three single-values multiplied by Dn scale
+
+
+
+#create an ellipsoid with a given FA
+#rotate it within the lab frame (i.e. slow compartment is tilted k-degrees in lab frame while fast compartment is tilted h-degrees in lab frame)
+def generate_rotated_anisotropic_ellipsoid(FA,compartment_rotations):
+    # FAs and abcs are just previously generated potentials to choose from
+    FAs = np.zeros(100)
+    abcs = np.empty([100,3])
+    for j in range(1,100): #creating an elipsoid
+        abc3 = [1,random.uniform(0.01,1),random.uniform(0.01,1)]
+        #print(abc3)
+        FAs[j] = (Calc_FA(abc3[0],abc3[1],abc3[2]))
+        abcs[j,:] = abc3
+    abc = GenerateFA_Ellipsoid(FA,FAs,abcs) #getting the ellipsoidal dimensions
+    a = abc[0] # i.e. Ax
+    b = abc[1] # i.e. By
+    c = abc[2] # i.e. Cz
+    #set up innitial 3d ellipsoid
+    A = np.array([[a,0,0],[0,b,0],[0,0,c]])
+    center = [0,0,0]
+    eigenvectors,eigenvalues,rotated = np.linalg.svd(A) #ignore 'rotated' as it's made in standard xyz, then will be rotated
+    theta = np.linspace(0.0, np.pi, 100) #cover all angles to build ellipsoid
+    phi = np.linspace(0.0, 2.0 * np.pi, 100)  #cover all angles to build ellipsoid
+    #get major axes
+    x = eigenvalues[0] * np.outer(np.cos(theta), np.sin(phi))
+    y = eigenvalues[1] * np.outer(np.sin(theta), np.sin(phi))
+    z = eigenvalues[2] * np.outer(np.ones_like(theta), np.cos(phi))
+    
+    #rotate by compartment_dependent lab-frame angles
+    rotation = Rxyz(compartment_rotations)
+    for i in range(len(x)):
+        for j in range(len(x)):
+            [x[i,j],y[i,j],z[i,j]] = np.dot([x[i,j],y[i,j],z[i,j]], rotation) + center
+    return x,y,z,abc
+
+
+#give all fractional anisotropies and their lab-frame rotations to generate the three compartment ellipsoids up front
+def CreateThreeEllipsoidalCompartments(All_FAs, All_rotations):
+    FA_fast, FA_med, FA_slow = All_FAs[0],All_FAs[1], All_FAs[2] # the anisotropies
+    fastrot,medrot,slowrot = All_rotations[0], All_rotations[1], All_rotations[2] #the original ellipsoid rotations (in the lab frame, so not all aligned in same direction)
+    
+    #create the three compartment ellipsoids rotated in lab-space, and then rotate along the b-value direction
+    x_fast,y_fast,z_fast,abc_fast = generate_rotated_anisotropic_ellipsoid(FA_fast,fastrot) # but then thetaxyz are same for fast, med, slow, because it's measured in the same lab-frame direction
+    x_med,y_med,z_med,abc_med = generate_rotated_anisotropic_ellipsoid(FA_med,medrot)
+    x_slow,y_slow,z_slow,abc_slow = generate_rotated_anisotropic_ellipsoid(FA_slow,slowrot)
+    
+    fast_compartment = [x_fast,y_fast,z_fast,abc_fast]
+    med_compartment = [x_med,y_med,z_med,abc_med]
+    slow_compartment = [x_slow,y_slow,z_slow,abc_slow]
+    
+    #show the 3 ellipsoids
+    fig = pl.figure()
+    ax = fig.add_subplot(131,projection='3d')
+    ax.plot_wireframe(x_fast, y_fast, z_fast,  rstride=4, cstride=4, color='b', alpha=0.2)
+    set_axes_equal(ax)
+    ax = fig.add_subplot(132,projection='3d')
+    ax.plot_wireframe(x_med, y_med, z_med,  rstride=4, cstride=4, color='g', alpha=0.2)
+    set_axes_equal(ax)
+    ax = fig.add_subplot(133,projection='3d')
+    ax.plot_wireframe(x_slow, y_slow, z_slow,  rstride=4, cstride=4, color='r', alpha=0.2)
+    set_axes_equal(ax)
+    #pl.savefig('/Users/neuroimaging/Desktop/Ellipsoid_Calc_FA41.png',format='png',dpi=120)
+    pl.show()
+    return fast_compartment, med_compartment, slow_compartment
+
+def RotateCompartments(x,y,z,thetaxyz):
+    center = [0,0,0]
+    rotation = Rxyz(thetaxyz)
+    for i in range(len(x)):
+        for j in range(len(x)):
+            [x[i,j],y[i,j],z[i,j]] = np.dot([x[i,j],y[i,j],z[i,j]], rotation) + center
+    return x,y,z
+
+#give true D, the three ellipsoidal compartments in labframe, and the b-value direction (i.e. thetaxyz)
+def GetOrthogonalD_thetas(D_traces,fast_compartment, med_compartment, slow_compartment, thetaxyz):
+    
+
+    #now measure along the given b-value direction
+    x_theta_fast,y_theta_fast,z_theta_fast = find_axes(fast_compartment[0],fast_compartment[1],fast_compartment[2]) #just get the length of the ellipsoid along the normal axis (i.e. 3 random orthogonal directions)
+    x_theta_med,y_theta_med,z_theta_med = find_axes(med_compartment[0],med_compartment[1],med_compartment[2]) #just get the length of the ellipsoid along the normal axis (i.e. 3 random orthogonal directions)
+    x_theta_slow,y_theta_slow,z_theta_slow = find_axes(slow_compartment[0],slow_compartment[1],slow_compartment[2]) #just get the length of the ellipsoid along the normal axis (i.e. 3 random orthogonal directions)
+
+    #now assume the single direction is measured along the x-axis (just so it's one direction)
+    x_thetas = [x_theta_fast, x_theta_med, x_theta_slow]
+    abc_s = [fast_compartment[3],med_compartment[3],slow_compartment[3]]
+    #get the new "diffusion values" i.e. the AxD_slow, AxD_med, AxD_fast
+    D_traces_b = Scale_Ds(D_traces,abc_s,x_thetas) 
+    
+    #now for comparison, also exporting the averaged across 3 values
+    fast_averaged = mean([x_theta_fast,y_theta_fast,z_theta_fast])
+    med_averaged = mean([x_theta_med,y_theta_med,z_theta_med])
+    slow_averaged = mean([x_theta_slow,y_theta_slow,z_theta_slow])
+    averaged_thetas = [fast_averaged, med_averaged, slow_averaged]
+    D_traces_b_averaged = Scale_Ds(D_traces,abc_s,averaged_thetas)
+
+    return D_traces_b,D_traces_b_averaged
+
+
+    
     
